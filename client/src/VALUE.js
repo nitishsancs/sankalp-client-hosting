fb322d0f284e4ffdce81dc98995e9e95d41e5a38
nitishsancs <nitishsancs@gmail.com>
2024-08-24 11:14:35 +0530

    aisnrimt

--- client/src/VALUE.js
+++ client/src/VALUE.js
@@ -1,2 +1,2 @@
 // export const backendIp = 'https://sld-test.onrender.com';
-export const backendIp = 'https://sankalp-server-hosting-a3g1v4o6o-nitishsancs-projects.vercel.app';
+export const backendIp = 'https://sankalp-server-hosting.vercel.app';
