54d09b25b72f9f03a4f6ff1fcb2c601ab14d46c4
nitishsancs <nitishsancs@gmail.com>
2024-08-24 16:22:13 +0530

    rsatxcarstc

--- client/src/VALUE.js
+++ client/src/VALUE.js
@@ -1,2 +1,2 @@
 // export const backendIp = 'https://sld-test.onrender.com';
-export const backendIp = 'https://sankalp-server-hosting-2b628rfci-nitishsancs-projects.vercel.app';
+export const backendIp = 'https://sankalp-server-hosting-rkthbzwje-nitishsancs-projects.vercel.app';
